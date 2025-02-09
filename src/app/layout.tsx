c4f86090c600372c416f86269067747c1bb734a2
a7medabdelfadil <a7med.abdelfadeel@gmail.com>
2025-02-09 14:56:27 +0200

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import "~/styles/globals.css";
 import { GeistSans } from "geist/font/sans";
 import "react-toastify/dist/ReactToastify.css";
@@ -9,23 +8,19 @@ import ThemeProvider from "./providers/themeProvider";
 import { usePathname } from "next/navigation";
 import NavBarMobileTop from "~/_components/navBarMobileTop";
 import NavBarMobileBottom from "~/_components/navBarMobileBottom";
-import ProtectedRoute from "~/_components/ProtectedRoute";
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   const pathname = usePathname();
-
-  const isAuthPage = [
-    "/sign-up",
-    "/sign-in",
-    "/forget-password",
-    "/reset-password",
-    "/change-password",
-  ].includes(pathname);
+  const isAuthPage =
+    pathname === "/sign-up" ||
+    pathname === "/sign-in" ||
+    pathname === "/forget-password" ||
+    pathname === "/reset-password" ||
+    pathname === "/change-password";
 
   const isEditProfile = pathname.includes("/edit-profile");
-
   return (
     <html
       lang="en"
@@ -37,35 +32,24 @@ export default function RootLayout({
         <meta name="description" content="Opream Person" />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, user-scalable=yes"
+          content="width=device-width, initial-scale=1.0, initial-scale=1.0, user-scalable=yes"
         />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       </head>
       <body className="bg-bgPrimary md:bg-bgSecondary">
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
-          <TRPCReactProvider>
-            {isAuthPage ? (
-              children
-            ) : (
-              <ProtectedRoute>
-                {!isAuthPage && (
-                  <>
-                    {/* Desktop NavBar */}
-                    <div className="hidden md:block">
-                      <NavBar />
-                    </div>
-
-                    {/* Mobile NavBar */}
-                    <div className="block md:hidden">
-                      {!isEditProfile && <NavBarMobileTop />}
-                      <NavBarMobileBottom />
-                    </div>
-                  </>
-                )}
-                <main>{children}</main>
-              </ProtectedRoute>
-            )}
-          </TRPCReactProvider>
+          {!isAuthPage && (
+            <>
+              <div className="hidden md:block">
+                <NavBar />
+              </div>
+              <div className="block md:hidden">
+                {!isEditProfile && <NavBarMobileTop />}
+                <NavBarMobileBottom />
+              </div>
+            </>
+          )}
+          <TRPCReactProvider>{children}</TRPCReactProvider>
         </ThemeProvider>
       </body>
     </html>
