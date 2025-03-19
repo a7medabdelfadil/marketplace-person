85086b3e4bab7b48583ba731dbb2b433538c3006
a7medabdelfadil <a7med.abdelfadeel@gmail.com>
2025-03-19 16:21:12 +0200

    fix

--- src/app/(auth)/verify-account/page.tsx
+++ src/app/(auth)/verify-account/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 /* eslint-disable @next/next/no-img-element */
 import React, { useState } from "react";
 import Button from "~/_components/Button";
